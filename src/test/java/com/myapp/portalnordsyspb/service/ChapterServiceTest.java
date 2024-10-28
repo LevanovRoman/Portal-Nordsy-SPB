ed74c40dfc007efdafe19f797b45f38d758259f9
levanovroman <levanovroman2016@yandex.ru>
2024-10-28 10:08:34 +0300

    add in dockerfile

--- src/test/java/com/myapp/portalnordsyspb/service/ChapterServiceTest.java
+++ src/test/java/com/myapp/portalnordsyspb/service/ChapterServiceTest.java
@@ -12,10 +12,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-
-import java.util.List;
 
 import static org.mockito.Mockito.when;
 
