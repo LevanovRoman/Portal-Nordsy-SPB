50750e9f6caa198d4f5e4ae404eb941021af17e5
Леванов Р.Г. <r.levanov@nordsy.spb.ru>
2024-11-01 10:32:38 +0300

    table 5s sorted

--- src/main/java/com/myapp/portalnordsyspb/library/service/SectionServiceImpl.java
+++ src/main/java/com/myapp/portalnordsyspb/library/service/SectionServiceImpl.java
@@ -1,7 +1,21 @@
 package com.myapp.portalnordsyspb.library.service;
 
+import com.myapp.portalnordsyspb.library.dto.request.SectionRequestDto;
+import com.myapp.portalnordsyspb.library.dto.response.SectionResponseDto;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Service
 public class SectionServiceImpl implements SectionService{
+    @Override
+    public SectionRequestDto addSection(SectionRequestDto sectionRequestDto, MultipartFile file) throws IOException {
+        return null;
+    }
+
+    @Override
+    public SectionResponseDto getSectionById(long sectionId) {
+        return null;
+    }
 }
